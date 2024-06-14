2868c42d262340c135f5d1afbc9743f0b29ebcc6
gskang <gskang2002@gmail.com>
2024-06-15 02:19:19 +0800

    added new skills + new pet froggy

---
+++ core/src/com/mygdx/game/entities/Froggy.java
@@ -0,0 +1,10 @@
+package com.mygdx.game.entities;
+
+public class Froggy extends Creature {
+    public Froggy() {
+        super(100, 100, "Froggy", "froggy.png");
+        skill1 = new Skill("Jump", 10);
+        skill2 = new Skill("Acid", 30);
+        skill3 = null;
+    }
+}
--- core/src/com/mygdx/game/entities/MeowmadAli.java
+++ core/src/com/mygdx/game/entities/MeowmadAli.java
@@ -6,7 +6,7 @@ public class MeowmadAli extends Creature{
     public MeowmadAli() {
         super(100, 100, "MeowmadAli", "meowmad_ali.png");
         skill1 = new Skill("Cat Scratch", 20);
-        skill2 = null;
+        skill2 = new Skill("Bite", 20);
         skill3 = null;
     }
 
