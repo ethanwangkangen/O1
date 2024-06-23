cc046554dc0e6754bf93bc2ae3b042653f69748e
gskang <windstick26@gmail.comtest>
2024-06-23 20:41:01 +0800

    added HealthBar class

---
+++ core/src/com/mygdx/global/HealthBar.java
@@ -0,0 +1,62 @@
+package com.mygdx.global;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.mygdx.game.entities.Creature;
+
+public class HealthBar extends ProgressBar {
+
+    /**
+     * creates a basic healthBar
+     * width 100 : height 20 ratio recommended
+     *
+     * @param width of the health bar
+     * @param height of the health bar
+     */
+
+    public HealthBar(int width, int height, Creature pet) {
+        super(0f, pet.getHealth(), 0.01f, false, new ProgressBarStyle());
+        getStyle().background = getColoredDrawable(width, height, Color.RED);
+        getStyle().knob = getColoredDrawable(0, height, Color.GREEN);
+        getStyle().knobBefore = getColoredDrawable(width, height, Color.GREEN);
+
+        setWidth(width);
+        setHeight(height);
+
+        setAnimateDuration(0.0f);
+        setValue(1f);
+
+        setAnimateDuration(0.25f);
+    }
+
+    public void setHealth(int health) {
+        super.setValue(health);
+    }
+
+    public Drawable getColoredDrawable(int width, int height, Color color) {
+
+        /**
+         * Creates an image of determined size filled with determined color.
+         *
+         * @param width of an image.
+         * @param height of an image.
+         * @param color of an image fill.
+         * @return {@link Drawable} of determined size filled with determined color.
+         */
+
+        Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+        pixmap.setColor(color);
+        pixmap.fill();
+
+        TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(new Texture(pixmap)));
+
+        pixmap.dispose();
+
+        return drawable;
+    }
+}
