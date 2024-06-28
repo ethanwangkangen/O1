451776921ae632efcd5f13ec7aa6ba950bc8364d
gskang <windstick26@gmail.comtest>
2024-06-28 15:25:40 +0800

    adding buffed pets for NPC

--- core/src/com/mygdx/game/entities/NPCDragon.java
+++ core/src/com/mygdx/game/entities/NPCDragon.java
@@ -6,7 +6,7 @@ public class NPCDragon extends Creature {
 
     public NPCDragon() {
 
-        super(100, 100, "Dragon", Creature.Element.FIRE);
+        super(1000, 1000, "Dragon", Creature.Element.FIRE);
         skill1 = new Skill("Firebreath", 20 * dmgFactor, Creature.Element.FIRE);
         skill2 = new Skill("Stomp", 50 * dmgFactor, Creature.Element.FIRE);
         skill3 = new Skill("Roast", 30 * dmgFactor, Creature.Element.FIRE);
@@ -17,5 +17,6 @@ public class NPCDragon extends Creature {
 
     @Override
     public void gainEXP(int i) {
+        // so that NPC does not level up
     }
 }
